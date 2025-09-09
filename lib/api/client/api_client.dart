474aa372960473411f78cd1dc92c8885ba6b9d8b
youssefmdev22 <youssefmdev22@gmail.com>
2025-09-09 19:13:48 +0300

    resolve conflicts

--- lib/api/client/api_client.dart
+++ lib/api/client/api_client.dart
@@ -1,18 +1,20 @@
 import 'package:dio/dio.dart';
-import 'package:elevate_ecommerce_app/api/models/requestes/register_request_dto/register_request_dto.dart';
-import 'package:elevate_ecommerce_app/api/models/responses/register_response_dto/register_response_dto.dart';
-import 'package:elevate_ecommerce_app/api/models/requestes/login_requests/login_request.dart';
-import 'package:elevate_ecommerce_app/api/models/responses/login_response/login_response_dto.dart';
-import 'package:elevate_ecommerce_app/core/constants/end_points.dart';
 import 'package:elevate_ecommerce_app/api/models/requestes/auth/forget_password_request.dart';
 import 'package:elevate_ecommerce_app/api/models/requestes/auth/reset_password_request.dart';
 import 'package:elevate_ecommerce_app/api/models/requestes/auth/verify_reset_request.dart';
+import 'package:elevate_ecommerce_app/api/models/requestes/login_requests/login_request.dart';
+import 'package:elevate_ecommerce_app/api/models/requestes/register_request_dto/register_request_dto.dart';
 import 'package:elevate_ecommerce_app/api/models/responses/auth/reset_password_response.dart';
 import 'package:elevate_ecommerce_app/api/models/responses/auth/verify_reset_response.dart';
+import 'package:elevate_ecommerce_app/api/models/responses/login_response/login_response_dto.dart';
+import 'package:elevate_ecommerce_app/api/models/responses/register_response_dto/register_response_dto.dart';
+import 'package:elevate_ecommerce_app/core/constants/end_points.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 
 import '../models/responses/auth/forget_password_response.dart';
+import '../models/responses/occasions_reponse_dto/occasions_reponse_dto.dart';
+import '../models/responses/products_reponse_dto/products_reponse_dto.dart';
 
 part 'api_client.g.dart';
 
@@ -24,34 +26,35 @@ abstract class ApiClient {
 
   @POST(Endpoints.register)
   Future<RegisterResponseDto> register(
-      @Body() RegisterRequestDto registerRequestDto,
-      );
+    @Body() RegisterRequestDto registerRequestDto,
+  );
 
   @POST(Endpoints.signIn)
   Future<LoginResponseDto> login(@Body() LoginRequestModel loginRequestModel);
 
-
   @POST(Endpoints.forgetPassword)
   Future<ForgetPasswordResponse> forgetPassword(
-      @Body() ForgetPasswordRequest body,
-      );
+    @Body() ForgetPasswordRequest body,
+  );
 
   @POST(Endpoints.verifyReset)
   Future<VerifyResetResponse> verifyResetCode(@Body() VerifyResetRequest body);
 
   @PUT(Endpoints.resetPassword)
-  Future<ResetPasswordResponse> resetPassword(@Body() ResetPasswordRequest body);
+  Future<ResetPasswordResponse> resetPassword(
+    @Body() ResetPasswordRequest body,
+  );
 
   @GET(Endpoints.occasions)
   Future<OccasionsReponseDto> getAllOccasions();
 
   @GET(Endpoints.products)
   Future<ProductsReponseDto> getProductsByOccasion(
-      @Query("occasion") String occasionId,
-      );
+    @Query(Endpoints.occasionQuery) String occasionId,
+  );
 
   @GET(Endpoints.products)
   Future<ProductsReponseDto> getProductsByCategory(
-      @Query("category") String categoryId,
-      );
+    @Query(Endpoints.categoryQuery) String categoryId,
+  );
 }
